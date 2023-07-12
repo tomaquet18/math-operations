53f594795a60ce88f891c28b5df1ffa4fbe2ce8a
tomaquet18 <tomaquet18@pop-os.localdomain>
2023-07-12 21:44:45 +0200

    Add sine, cosine and tangent

---
+++ trigonometry/trigonometry.py
@@ -0,0 +1,10 @@
+import math
+
+def calculate_sine(angle):
+    return math.sin(math.radians(angle))
+
+def calculate_cosine(angle):
+    return math.cos(math.radians(angle))
+
+def calculate_tangent(angle):
+    return math.tan(math.radians(angle))
